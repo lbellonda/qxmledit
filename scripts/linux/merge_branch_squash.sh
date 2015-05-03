37a048b77071e813bd8c3c6070860e222d289e7d
Luca Bellonda <lbellonda@gmail.com>
2015-05-03 09:23:01 +0200

    Scripts for git development.

---
+++ scripts/linux/merge_branch_squash.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+export BRANCH=$1
+if [ -z $BRANCH  ]; then
+    echo "branch not set"
+    exit -1
+fi
+
+echo "merging and squashing the branch $1"
+git checkout master
+if [ $? -ne 0 ]; then
+    echo "cannot switch to master"
+    exit -1
+fi
+git merge --squash $BRANCH
+
+
