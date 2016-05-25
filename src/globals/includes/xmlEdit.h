d5578f04bca4ac252d85ef37c12dd820987f5a8d
Luca Bellonda <lbellonda@gmail.com>
2016-05-25 22:26:51 +0200

    Snapshot on

--- src/globals/includes/xmlEdit.h
+++ src/globals/includes/xmlEdit.h
@@ -31,7 +31,7 @@
 // Version numbers are divided like that: 0x0005.02.00
 
 //------------------------------------------------
-//#define QXMLEDIT_VERSION_IS_SNAPSHOT 1
+#define QXMLEDIT_VERSION_IS_SNAPSHOT 1
 #if defined(QXMLEDIT_VERSION_IS_SNAPSHOT)
 #define VERSION_BRANCH "-SNAPSHOT"
 #else
